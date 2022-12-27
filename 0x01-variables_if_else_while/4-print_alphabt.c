b6141a70048edefb9f9781cf786816db4691302c
Dit123 <denhaitulua@gmail.com>
2022-12-27 06:55:25 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - return alphabets both except e and q
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
